a6041438ccd8a3502b19cce7fb04a47222397c8e
Mihail Binev <kodeart@gmail.com>
2017-04-19 10:57:09 +0200

    Adds license header

--- Uri.php
+++ Uri.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the Koded package.
+ *
+ * (c) Mihail Binev <mihail@kodeart.com>
+ *
+ * Please view the LICENSE distributed with this source code
+ * for the full copyright and license information.
+ *
+ */
+
 namespace Koded\Http;
 
 use InvalidArgumentException;
