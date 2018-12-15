89c5aaa25f61d9ddd3572c31fb2498ab25950928
Mihail Binev <kodeart@gmail.com>
2018-12-15 07:25:01 +0100

    Typos

--- Tests/Integration/RequestIntegrationTest.php
+++ Tests/Integration/RequestIntegrationTest.php
@@ -11,14 +11,14 @@ class RequestIntegrationTest extends \Http\Psr7Test\RequestIntegrationTest
     protected $request;
 
     protected $skippedTests = [
-        'testUri'                              => 'Skipped because og the host requirement',
+        'testUri'                              => 'Skipped because of the host requirement',
         'testMethod'                           => 'Implementation uses constants (capitalisation matters)',
         'testMethodWithInvalidArguments'       => 'Does not make sense for strict type implementation',
         'testWithHeaderInvalidArguments'       => 'Does not make sense for strict type implementation',
         'testWithAddedHeaderInvalidArguments'  => 'Does not make sense for strict type implementation',
-        'testWithAddedHeaderArrayValueAndKeys' => 'Skip, the test id weird',
-        'testWithAddedHeader'                  => 'Skip, the test id weird',
-        'testUriPreserveHost_NoHost_Host'      => 'Skipped because og the host requirement',
+        'testWithAddedHeaderArrayValueAndKeys' => 'Skip, the test is weird',
+        'testWithAddedHeader'                  => 'Skip, the test is weird',
+        'testUriPreserveHost_NoHost_Host'      => 'Skipped because of the host requirement',
     ];
 
     /**
