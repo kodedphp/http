1676057775c368277f9c604885cd876f9a937cf9
Mihail Binev <kodeart@gmail.com>
2020-09-10 11:26:36 +0200

    - matches() can return all matched headers - match returns only one resolved header object

--- AcceptHeaderNegotiator.php
+++ AcceptHeaderNegotiator.php
@@ -38,7 +38,7 @@ class AcceptHeaderNegotiator
         $this->supports = $supportHeader;
     }
 
-    public function match(string $accepts, bool $all = false): AcceptHeader
+    public function match(string $accepts): AcceptHeader
     {
         /** @var AcceptHeader $support */
         foreach ($this->parse($accepts) as $accept) {
@@ -59,7 +59,31 @@ class AcceptHeaderNegotiator
              */
             $types[] = new class('*;q=0') extends AcceptHeader {};
         }
-        return $all ? $types : $types[0];
+        return $types[0];
+    }
+
+    public function matches(string $accepts): array
+    {
+        /** @var AcceptHeader $support */
+        foreach ($this->parse($accepts) as $accept) {
+            foreach ($this->parse($this->supports) as $support) {
+                $support->matches($accept, $types);
+            }
+        }
+        usort($types, function(AcceptHeader $a, AcceptHeader $b) {
+            return $b->weight() <=> $a->weight();
+        });
+        if (empty($types)) {
+            /* Set "q=0", meaning the header is explicitly rejected.
+             * The consuming clients should handle this according to
+             * their internal logic. This is much better then throwing
+             * exceptions which must be handled in every place where
+             * match() is called. For example, the client may issue a
+             * 406 status code and be done with it.
+             */
+            $types[] = new class('*;q=0') extends AcceptHeader {};
+        }
+        return $types;
     }
 
     /**
